6ac83affd40a291474920eac530b66a24066a36c
Matti Räty <mattiraty@hotmail.com>
2023-01-23 14:30:25 +0200

    working on script

--- script/script.js
+++ script/script.js
@@ -4,3 +4,38 @@
 
 console.log('Hello there!');
 
+window.addEventListener('load', () => {
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector('#new-task-input');
+    const listElements = document.querySelector('#tasks');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if (!task) {
+            alert('Please add a task');
+            return;
+        }
+
+        const taskElement = document.createElement('div');
+        taskElement.classList.add('task');
+
+        const taskContentElement = document.createElement('div');
+        taskContentElement.classList.add('content');
+        
+
+        taskElement.appendChild(taskContentElement);
+
+        const taskInputElement = document.createElement('input');
+        taskInputElement.classList.add('text');
+        taskInputElement.type = 'text';
+        taskInputElement.value = task;
+        taskInputElement.setAttribute('readonly', 'readonly');
+
+        taskContentElement.appendChild(taskInputElement);
+
+        listElements.appendChild(taskElement);
+    })
+})
